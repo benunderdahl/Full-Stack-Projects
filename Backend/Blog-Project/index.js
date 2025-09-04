d128cb7f7190f0273ada77e12dc43bb2eb61a3f7
benunderdahl <benjamin.underdahl@proton.me>
2025-09-03 21:36:14 -0500

    did some more work on the blog project

--- Backend/Blog-Project/index.js
+++ Backend/Blog-Project/index.js
@@ -6,22 +6,12 @@ import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
-
+app.use(express.static("public"))
 
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
-
-
-
-
-
-
-
-
-
-
 
 
 
